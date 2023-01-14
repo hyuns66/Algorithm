24a0bb78fbbecf019d70b83faa0347f9dbd77211
lee-june-young <uhiccup@gmail.com>
2023-01-14 22:40:46 +0900

    01/14: Solve String BOJ6550[부분문자열]

---
+++ string/JunYoung/BOJ_6550.py
@@ -0,0 +1,55 @@
+# 부분 문자열 (실버5)
+
+import sys
+from collections import deque
+
+result = []
+f = open("BOJ_6550_input.txt", 'r')
+lines = f.readlines()
+for line in lines:
+    s, t = map(str, line.strip().split())
+    s = deque(s)
+
+    for i in range(0, len(t)):
+        if len(s) == 0:
+            break
+
+        if t[i] == s[0]:
+            s.popleft()
+
+    if len(s) == 0:
+        result.append("Yes")
+    else:
+        result.append("No")
+
+for i in result:
+    print(i)
+f.close()
+
+"""
+# 부분 문자열 (실버5)
+
+import sys
+from collections import deque
+
+result = []
+lines = sys.stdin.readlines()
+for line in lines:
+    s, t = map(str, line.strip().split())
+    s = deque(s)
+
+    for i in range(0, len(t)):
+        if len(s) == 0:
+            break
+            
+        if t[i] == s[0]:
+            s.popleft()
+     
+    if len(s) == 0:
+        result.append("Yes")
+    else:
+        result.append("No")
+
+for i in result:
+    print(i)
+"""
