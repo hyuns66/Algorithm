7eef3fb4fc09c5158d5352efaa33204989126029
lee-june-young <uhiccup@gmail.com>
2023-01-29 23:10:47 +0900

    01/29 Solving String BOJ_14405[피카츄]

---
+++ string/JunYoung/BOJ_14405.py
@@ -0,0 +1,23 @@
+# 피카츄
+
+import sys
+
+S = sys.stdin.readline().strip()
+
+i = 0
+while True:
+    if i > len(S)-1:
+        break
+    if i+2 < len(S) and S[i] == 'p' and S[i + 1] == 'i':
+        i += 2
+    elif i+2 < len(S) and S[i] == 'k' and S[i + 1] == 'a':
+        i += 2
+    elif i+3 < len(S) and S[i] == 'c' and S[i + 1] == 'h' and S[i + 2] == 'u':
+        i += 3
+    else:
+        break
+
+if i > len(S)-1:
+    print("YES")
+else:
+    print("NO")
