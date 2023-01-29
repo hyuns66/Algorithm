3544a537b3c6ff46a41265ee6bcf31774d9de7a2
lee-june-young <uhiccup@gmail.com>
2023-01-29 23:18:34 +0900

    01/29 Solve String BOJ_14405[피카츄]

--- string/JunYoung/BOJ_14405.py
+++ string/JunYoung/BOJ_14405.py
@@ -8,11 +8,11 @@ i = 0
 while True:
     if i > len(S)-1:
         break
-    if i+2 < len(S) and S[i] == 'p' and S[i + 1] == 'i':
+    if i+2 <= len(S) and S[i] == 'p' and S[i + 1] == 'i':
         i += 2
-    elif i+2 < len(S) and S[i] == 'k' and S[i + 1] == 'a':
+    elif i+2 <= len(S) and S[i] == 'k' and S[i + 1] == 'a':
         i += 2
-    elif i+3 < len(S) and S[i] == 'c' and S[i + 1] == 'h' and S[i + 2] == 'u':
+    elif i+3 <= len(S) and S[i] == 'c' and S[i + 1] == 'h' and S[i + 2] == 'u':
         i += 3
     else:
         break
