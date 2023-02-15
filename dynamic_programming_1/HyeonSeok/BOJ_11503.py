7cf0035db41ac1b3a7ff693db3a9cb899bc6c455
Hyuns66 <hyuns6677@gmail.com>
2023-02-15 22:32:26 +0900

    2023-02-15

--- dynamic_programming_1/HyeonSeok/BOJ_11503.py
+++ dynamic_programming_1/HyeonSeok/BOJ_11503.py
@@ -3,8 +3,17 @@ import sys
 N = int(sys.stdin.readline())
 sequence = list(map(int, sys.stdin.readline().split()))
 mem = [1] * N
-
+answer = 1
 for i in range(1, N):
     cnt = 1
+    flag = False
     for j in range(i):
-        cnt = max(cnt, mem[j])
+        if sequence[j] < sequence[i]:
+            cnt = max(cnt, mem[j])
+            flag = True
+    if flag:
+        cnt += 1
+    mem[i] = cnt
+    answer = max(answer, cnt)
+
+print(answer)
