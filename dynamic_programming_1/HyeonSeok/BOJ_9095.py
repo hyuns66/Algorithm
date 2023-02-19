6f459ad312a52a058236bcf57207ffd9506d2b71
Hyuns66 <hyuns6677@gmail.com>
2023-02-19 16:01:19 +0900

    2023-02-19

---
+++ dynamic_programming_1/HyeonSeok/BOJ_9095.py
@@ -0,0 +1,17 @@
+import sys
+
+answer = list()
+T = int(sys.stdin.readline())
+# 각 테스트 케이스에 대해 시행
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    dp = [0] * n
+    for i in range(n):
+        if i+1 <= 3:
+            dp[i] = 1
+        for j in range(1, 4):
+            if i-j >= 0:
+                dp[i] += dp[i-j]
+    answer.append(dp[-1])
+for a in answer:
+    print(a)
