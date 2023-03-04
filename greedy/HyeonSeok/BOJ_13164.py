908c5c7fa1bea22f6741064551cda9b99e09a4b0
Hyuns66 <hyuns6677@gmail.com>
2023-03-04 18:53:17 +0900

    2023-03-04

--- greedy/HyeonSeok/BOJ_13164.py
+++ greedy/HyeonSeok/BOJ_13164.py
@@ -41,40 +41,19 @@
 N-K만큼 반복하며 가장 차이가 작은 둘씩 짝지음
 """
 import sys
-import heapq
 
 N, K = map(int, sys.stdin.readline().split())
 children = list(map(int, sys.stdin.readline().split()))
 visited = [False] * (N-1)
-h = list()
+height_diff = list()
 answer = 0
 
-for i in range(N-2):
-    heapq.heappush(h, (children[i+1] - children[i], i, i+1))
+for i in range(N-1):
+    height_diff.append(children[i+1] - children[i])
 
 # 키차이를 이용해서 정렬 후 그리디알고리즘 적용
-for _ in range(N-K):
-    data = heapq.heappop(h)
-    print(data)
-    start = data[1]
-    end = data[2]
-    if visited[start] and visited[end]:
-        continue
-    elif not visited[start] and not visited[end]:
-        visited[start] = True
-        visited[end] = True
-        answer += data[0]
-    else:
-        if visited[start]:
-
-    while pos > 0:
-        pos -= 1
-        if not visited[pos]:
-            break
-    heapq.heappush(h, (children[data[2]] - children[pos], pos, data[2]))
-
-for _ in range(K):
-    data = heapq.heappop(h)
-    answer += data[0]
+height_diff.sort()
+for i in range(N-K):
+    answer += height_diff[i]
 
 print(answer)
