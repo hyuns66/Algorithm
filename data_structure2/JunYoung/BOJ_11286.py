a2cc090a1c9ecf92aa9ff9d39328988633935182
lee-june-young <uhiccup@gmail.com>
2023-03-04 19:23:03 +0900

    03/04 Solving DataStructure2 BOJ_11286[절대값 힙]

--- data_structure2/JunYoung/BOJ_11286.py
+++ data_structure2/JunYoung/BOJ_11286.py
@@ -1,1 +1,35 @@
 # 절댓값 힙
+
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+
+negativeHeap = []
+positiveHeap = []
+answer = []
+
+for i in range(N):
+    calculation = int(sys.stdin.readline())
+    if calculation != 0:
+        if calculation < 0:
+            heapq.heappush(negativeHeap, -calculation)  # 최대 힙
+        else:
+            heapq.heappush(positiveHeap, calculation)  # 최소 힙
+    else:
+        #print(negativeHeap)
+        #print(positiveHeap)
+        if len(negativeHeap) != 0 and len(positiveHeap) != 0:
+            if negativeHeap[0]<=positiveHeap[0]:
+                answer.append(-(heapq.heappop(negativeHeap)))
+            else:
+                answer.append((heapq.heappop(positiveHeap)))
+        elif len(negativeHeap) != 0:
+            answer.append(-(heapq.heappop(negativeHeap)))
+        elif len(positiveHeap) != 0:
+            answer.append((heapq.heappop(positiveHeap)))
+        else:
+            answer.append(0)
+
+for i in answer:
+    print(i)
