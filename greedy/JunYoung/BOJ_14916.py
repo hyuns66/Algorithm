27994a1c1b6618e3fbd18f9fca2594fdfba85d2a
lee-june-young <uhiccup@gmail.com>
2023-04-09 21:24:27 +0900

    04/09 Solve Greedy BOJ_14916[거스름돈]

--- greedy/JunYoung/BOJ_14916.py
+++ greedy/JunYoung/BOJ_14916.py
@@ -1,1 +1,23 @@
 # 거스름돈
+
+import sys
+
+n = int(sys.stdin.readline())
+
+coins = 0
+
+while True:
+
+    if n == 0:
+        print(coins)
+        break
+    elif n < 2:
+        print(-1)
+        break
+    elif n % 5 == 0:
+        n -= 5
+        coins += 1
+    else:
+        n -= 2
+        coins += 1
+
