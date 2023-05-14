46440d736c8e8b3011e3b709a2a1213e195c7eff
lee-june-young <uhiccup@gmail.com>
2023-05-14 22:58:16 +0900

    0514 Solve Greedy BOJ_1541[잃어버린 괄호]

---
+++ greedy/JunYoung/BOJ_1541.py
@@ -0,0 +1,48 @@
+# 잃어버린 괄호
+
+import sys
+from collections import deque
+
+exp = sys.stdin.readline().strip()
+newexp = ['0' for i in range(0, len(exp))]
+flag = 0
+for i in range(0, len(exp)):
+    if flag == 0:
+        if exp[i] == '-':
+            flag = 1
+        newexp[i] = exp[i]
+    else:
+        if exp[i] == '+':
+            newexp[i] = '-'
+        else:
+            newexp[i] = exp[i]
+
+#print(newexp)
+
+num = deque()
+math = deque()
+temp = ''
+for i in newexp:
+    if i == '+' or i =='-':
+        num.append(int(temp))
+        math.append(i)
+        temp = ''
+    else:
+        temp += i
+
+num.append(int(temp))
+
+#print(num)
+#print(math)
+
+for i in math:
+    if i == '+':
+        a = num.popleft()
+        b = num.popleft()
+        num.appendleft(a+b)
+    else:
+        a = num.popleft()
+        b = num.popleft()
+        num.appendleft(a - b)
+
+print(num.pop())
