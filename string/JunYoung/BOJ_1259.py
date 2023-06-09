4145924bc9c277b6e88f459e13ae788cdda96ffd
lee-june-young <uhiccup@gmail.com>
2023-06-10 00:01:17 +0900

    0610 Solve String BOJ_1259[팰린드롬수]

--- string/JunYoung/BOJ_1259.py
+++ string/JunYoung/BOJ_1259.py
@@ -3,6 +3,7 @@
 import sys
 import math
 
+answer = []
 while True:
     line = sys.stdin.readline().strip()
     if line == '0':
@@ -12,5 +13,10 @@ while True:
         if (line[i]!=line[len(line)-1-i]):
             flag = 0
             break
+    if flag:
+        answer.append("yes")
+    else:
+        answer.append("no")
 
-
+for i in answer:
+    print(i)
