45d25707e1b15cad31529018fa88d310c9bc8d20
Hyuns66 <hyuns6677@gmail.com>
2023-08-03 17:08:23 +0900

    2023-08-03

--- dynamic_programming_1/HyeonSeok/BOJ_22869.py
+++ dynamic_programming_1/HyeonSeok/BOJ_22869.py
@@ -1,13 +1,13 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-num_list = list()
-for _ in range(N):
-    num_list.append(int(sys.stdin.readline()))
-dp = [0]*(K+1)
-for num in num_list:
-    if num <= K:
-        dp[num] += 1
-    for i in range(num+1, K+1):
-        dp[i] += dp[i - num]
-print(dp[-1])
+num_list = list(map(int, sys.stdin.readline().split()))
+dp = [False] * N
+dp[0] = True
+for i in range(N):
+    if not dp[i]:
+        continue
+    for dist in range(1, K+1):
+        if i + dist < N and dist * (1 + abs(num_list[i] - num_list[i + dist])) <= K:
+            dp[i + dist] = True
+print('YES' if dp[-1] else 'NO')
