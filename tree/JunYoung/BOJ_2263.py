18f562f0664a84632fd39239ec5f3705bfefc12f
lee-june-young <uhiccup@gmail.com>
2024-01-13 17:39:18 +0900

    0113 Edit Tree BOJ_2263

--- tree/JunYoung/BOJ_2263.py
+++ tree/JunYoung/BOJ_2263.py
@@ -5,17 +5,17 @@ import sys
 sys.setrecursionlimit(10 ** 5)
 
 
-def preorder(start, end, s, e):
-    if start > end:
+def preorder(in_start, in_end, post_start, post_end):
+    if in_start > in_end:
         return
-    if s > e:
+    if post_start > post_end:
         return
-    root = postorder[e]
+    root = postorder[post_end]
     idx = inorder.index(root)  # 루트의 인덱스
-    left_len = idx - start
+    left_len = idx - in_start
     answer.append(root)  # 루트
-    preorder(start, start + left_len - 1, s, s + left_len - 1)  # 왼쪽 서브트리
-    preorder(start + left_len + 1, end, s + left_len, e - 1)  # 오른쪽 서브트리
+    preorder(in_start, in_start + left_len - 1, post_start, post_start + left_len - 1)  # 왼쪽 서브트리
+    preorder(in_start + left_len + 1, in_end, post_start + left_len, post_end - 1)  # 오른쪽 서브트리
 
 
 n = int(sys.stdin.readline())
