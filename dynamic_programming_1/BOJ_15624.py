64db1848391cc2c17fad997c10d9489d5a7fbf21
lee-june-young <uhiccup@gmail.com>
2024-04-15 21:00:09 +0900

    0415 Solve dp BOJ_15624

---
+++ dynamic_programming_1/BOJ_15624.py
@@ -0,0 +1,16 @@
+# 피보나치 수 7
+
+import sys
+
+n = int(sys.stdin.readline())
+memo = [0 for _ in range(n+2)] #n+2까지로 해야 n+1만큼 만들어줌
+
+memo[0] = 0
+memo[1] = 1
+
+for i in range(2, n+1):
+    memo[i] = (memo[i-2] + memo[i-1]) % 1000000007
+
+print(memo[n])
+
+## dp값 저장할때 나머지 연산을 한 결과를 저장! 안그러면 너무 큰수가 많이 저장돼 메모리 초과 난다.
