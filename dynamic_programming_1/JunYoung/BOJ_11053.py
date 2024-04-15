1b20334e6eaf7b3f00b894a7ef8511107c986c6f
lee-june-young <uhiccup@gmail.com>
2024-04-16 01:38:15 +0900

    comment on BOJ_11053

--- dynamic_programming_1/JunYoung/BOJ_11053.py
+++ dynamic_programming_1/JunYoung/BOJ_11053.py
@@ -17,3 +17,4 @@ print(max(dp))
 
 #dp로 풀면 O(N^2) -> 이분 탐색 O(NlogN)
 #https://velog.io/@black_han26/%EB%B0%B1%EC%A4%80Python-11053-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4-LIS
+#https://great-park.tistory.com/29
