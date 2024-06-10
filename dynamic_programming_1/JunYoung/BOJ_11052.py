ed9c881872f970ddc868bef39f22ca7f6ba19e91
lee-june-young <uhiccup@gmail.com>
2024-06-10 18:34:01 +0900

    Update comments on DP BOJ_11052

--- dynamic_programming_1/JunYoung/BOJ_11052.py
+++ dynamic_programming_1/JunYoung/BOJ_11052.py
@@ -15,3 +15,7 @@ for i in range(N): # 각 카드 N개를 얻기 위해 지불해야하는 최대 
     #print(dp)
 
 print(dp[-1])
+
+# 인덱스 맞춰주는게 어려웠다..!!!!!
+# array랑 dp 둘다 인덱스 1부터 사용하는 편이 만들기가 더 쉬울 것이다!!
+# array의 경우 [0] + list(map(int, sys.stdin.readline().split()))해주면 쉽게 인덱스 1부터 사용가능
